f044106e3ffa482632df5273d9b21f2152d89e4a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-07 02:00:59 +0600

    middleware code

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
     const {username,email,password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
@@ -20,7 +20,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json("Signup Success");
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
     }
    
 }
