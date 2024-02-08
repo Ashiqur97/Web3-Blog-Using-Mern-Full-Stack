7e3127372de80610d21ddb85484ddcf45d829cd9
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-09 01:21:38 +0600

    footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,44 @@
-import { Footer } from "flowbite-react"
-import { Link } from "react-router-dom";
-
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
 export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
-        <div className="w-full max-w-7xl mx-auto">
-            <div className="grid w-full justify-between sm:flex md:grid-cols-1">
-                <div className="mt-5">
-                <Link
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
               to='/'
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-blue-500 rounded-lg text-white'>
-                Web3 
+                Web3
               </span>
               Blog
             </Link>
-                </div>  
-                <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
-                    <Footer.Title title='About' />
-                    <Footer.LinkGroup col>
-                    <Footer.Link
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
                   href='https://github.com/Ashiqur97'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  BlockChain Audit and Web Projects
+                  BlockChain Audit and Web Projects 
                 </Footer.Link>
                 <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Web3 Blog
+                  Web3 Blog Article 
                 </Footer.Link>
-                </Footer.LinkGroup>
-                </div>
+              </Footer.LinkGroup>
             </div>
-            <Footer.Title title='Follow us' />
+            <div>
+              <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
                   href='https://github.com/Ashiqur97'
@@ -48,7 +49,32 @@ export default function FooterCom() {
                 </Footer.Link>
                 <Footer.Link href='#'>Discord</Footer.Link>
               </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
         </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Web3 blog Article"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/Ashiqur97' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsDribbble}/>
+
+          </div>
+        </div>
+      </div>
     </Footer>
-  )
+  );
 }
