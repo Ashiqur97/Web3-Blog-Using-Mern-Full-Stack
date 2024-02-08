509f520da4446e83cd6b7b80178012e0c805c55c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-08 18:50:10 +0600

    code commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,14 @@ import { Link } from "react-router-dom"
 import { TextInput,Label, Button } from "flowbite-react"
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({})
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+    console.log(formData)
+  }
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -19,13 +27,14 @@ export default function SignUp() {
           </p>
           </div>
           <div className="flex-1">
-            <form className='flex flex-col gap-4'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
               <div>
                 <Label value='Full Name' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
+                onChange={}
               />
               </div>
 
@@ -35,6 +44,7 @@ export default function SignUp() {
                 type='text'
                 placeholder='user@gmail.com'
                 id='email'
+                onChange={}
               />
               </div>
 
@@ -44,6 +54,7 @@ export default function SignUp() {
                 type='text'
                 placeholder='password'
                 id='password'
+                onChange={}
               />
               </div>
               <Button gradientDuoTone="purpleToBlue" type="submit">
