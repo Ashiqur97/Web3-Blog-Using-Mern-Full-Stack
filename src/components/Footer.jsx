3436fa39c7e954fa24c337d7ea8fc442ae639571
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-09 01:14:09 +0600

    code commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,9 +19,35 @@ export default function FooterCom() {
                 </div>  
                 <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
                     <Footer.Title title='About' />
-                    
+                    <Footer.LinkGroup col>
+                    <Footer.Link
+                  href='https://github.com/Ashiqur97'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  BlockChain Audit and Web Projects
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Web3 Blog
+                </Footer.Link>
+                </Footer.LinkGroup>
                 </div>
             </div>
+            <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://github.com/Ashiqur97'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='#'>Discord</Footer.Link>
+              </Footer.LinkGroup>
         </div>
     </Footer>
   )
