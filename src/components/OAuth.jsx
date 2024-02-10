3a4ca5e9d82d69250174e4c9c444d9369b48352c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-11 00:38:40 +0600

    code oauth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { Button } from "flowbite-react"
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { GoogleAuthProvider } from "firebase/auth";
 
 export default function OAuth() {
+    const handleGoogleClick = async() => {
+        const provider = new GoogleAuthProvider();
+    }
   return (
-    <div>
-      OAuth 
-    </div>
+    <Button type="button" gradientDuoTone="blue" outline onClick={handleGoogleClick}>
+        <AiFillGoogleCircle size={24} />
+        Login With Google
+    </Button>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY ,
+  authDomain: "mern-blog-94ea2.firebaseapp.com",
+  projectId: "mern-blog-94ea2",
+  storageBucket: "mern-blog-94ea2.appspot.com",
+  messagingSenderId: "396067025288",
+  appId: "1:396067025288:web:5c14fb4afa5c3ee38d4a3d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
