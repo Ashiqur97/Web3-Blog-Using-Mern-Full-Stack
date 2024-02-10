a07ad5b5544195ad19896d944d552f0ed2284853
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-11 01:28:43 +0600

    code googleauth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,19 +1,20 @@
-/* eslint-disable no-unused-vars */
-import { Button } from "flowbite-react"
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { GoogleAuthProvider,signInWithPopup,getAuth } from "firebase/auth";
-import { app } from "../firebase";
-import { UseDispatch } from "react-redux";
+import { Button } from 'flowbite-react';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-    const auth = getAuth(app);
-    const dispatch = UseDispatch();
-    const handleGoogleClick = async() => {
+    const auth = getAuth(app)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleGoogleClick = async () =>{
         const provider = new GoogleAuthProvider()
-        provider.setCustomParameters({ prompt: 'select_account' });
+        provider.setCustomParameters({ prompt: 'select_account' })
         try {
-            const resultsFromGoogle = await signInWithPopup(auth, provider);
+            const resultsFromGoogle = await signInWithPopup(auth, provider)
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -22,19 +23,20 @@ export default function OAuth() {
                     email: resultsFromGoogle.user.email,
                     googlePhotoUrl: resultsFromGoogle.user.photoURL,
                 }),
-                });
-                const data = await res.json();
-                if(res.ok){
-                    dispatch(signInSuccess(data));
-                }
+                })
+            const data = await res.json()
+            if (res.ok){
+                dispatch(signInSuccess(data))
+                navigate('/')
+            }
         } catch (error) {
             console.log(error);
         }
-    }
+    } 
   return (
-    <Button type="button" gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
-        <AiFillGoogleCircle size={24} />
-        Login With Google
+    <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+        <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
+        Login with Google
     </Button>
   )
 }
