13204d52fedeb89f1e6342c76ca901cc9c1cb433
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-11 23:27:32 +0600

    code commit

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,18 +1,31 @@
 import { Sidebar } from 'flowbite-react';
 import {HiArrowSmRight, HiUser} from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import {Link, useLocation } from 'react-router-dom';
 
 export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if (tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    }, [location.search]);
   return (
     <Sidebar>
       <Sidebar.Items>
-        <Sidebar.Group>
-            <Sidebar.Item active icon={HiUser} label={'User'} labelColor='dark'>
+        <Sidebar.ItemGroup>
+            <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark'>
                 Profile 
             </Sidebar.Item>
-            <Sidebar.Item active icon={HiArrowSmRight} label={'User'} labelColor='dark'>
+            </Link>
+            <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
                 Signout 
             </Sidebar.Item>
-        </Sidebar.Group>
+        </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
   )
