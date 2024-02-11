e8c92b0ab8effcf88418ea6abb189594b0486813
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-11 13:06:26 +0600

    initial code user data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { Link,useLocation } from 'react-router-dom';
-import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Navbar, TextInput, Button, Dropdown, Avatar } from 'flowbite-react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector} from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user)
 
   return (
     <Navbar className='border-b-2'>
@@ -29,11 +32,27 @@ export default function Header() {
           pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline 
+              label= {
+                <Avatar 
+                  alt='user'
+                  img={currentUser.profilePicture}
+                  rounded
+                />
+              }
+            >
+
+            </Dropdown>
+          ) : (
+            <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+          )}
         <Navbar.Toggle />
       </div>
 
