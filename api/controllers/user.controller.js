9b2383e7405f134e9b0f77d08548ee099f2de2b9
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-13 14:51:02 +0600

    code update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
-import { errorHandler } from "../utils/error";
+import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
     res.json({ message: 'API is working fine!' });
@@ -33,4 +34,22 @@ export const updateUser = async (req, res, next) => {
             );
           }
     }
-}
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.userId,
+          {
+            $set: {
+              username: req.body.username,
+              email: req.body.email,
+              profilePicture: req.body.profilePicture,
+              password: req.body.password,
+            },
+          },
+          { new: true }
+        );
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+      } catch (error) {
+        next(error);
+      }
+    };
