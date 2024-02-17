b3a597491e182288748680c2e6914a5f07deac01
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-17 22:50:44 +0600

    create post page ui

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -10,9 +10,10 @@ import { updateStart,updateSuccess,updateFailure,
   deleteUserFailure,deleteUserStart,deleteUserSuccess,signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-  const {currentUser,error} = useSelector(state => state.user)
+  const {currentUser,error,loading} = useSelector(state => state.user)
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -219,7 +220,20 @@ export default function DashProfile() {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>Update</Button>
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...' : 'Update'}
+          </Button>
+          {
+            currentUser.isAdmin && (
+              <Link to={'/create-post'}>
+                      <Button type="button" 
+              gradientDuoTone='purpleToBlue' className="w-full">
+                Create a post 
+              </Button>
+              </Link>
+        
+            )
+          }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>
