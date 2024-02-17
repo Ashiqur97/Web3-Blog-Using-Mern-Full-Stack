ca05042a6773092b9eac24b4a39d4171c20e2736
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-17 21:56:14 +0600

    code signout functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,13 +3,16 @@ import {
   deleteUser,
   test,
   updateUser,
+  signout,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+// import { sign } from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post('/signout',signout);
 
 export default router;
