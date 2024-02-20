898968be921a2c6d210d122337bf890f75c6ac43
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-20 15:26:11 +0600

    complete create post functionality

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -20,6 +20,7 @@ export default function CreatePost() {
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
+
   const navigate = useNavigate();
 
   const handleUpdloadImage = async () => {
@@ -58,6 +59,30 @@ export default function CreatePost() {
       console.log(error);
     }
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
@@ -79,9 +104,9 @@ export default function CreatePost() {
             }
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='javascript'>Web3 Basic and solidity</option>
+            <option value='reactjs'>Defi And Ethereum</option>
+            <option value='nextjs'>Smart Contract Audit</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
@@ -118,22 +143,24 @@ export default function CreatePost() {
             className='w-full h-72 object-cover'
           />
         )}
-            <ReactQuill theme="snow" placeholder="Write Something" 
-            className='h-72 mb-12' 
-            required 
-            onChange={(value) => {
-                setFormData({ ...formData, content: value });
-            }}
-            />
-            <Button type="submit" gradientDuoTone='purpleToBlue'>
-                Publish
-            </Button>
-            {publishError && (
+        <ReactQuill
+          theme='snow'
+          placeholder='Write something...'
+          className='h-72 mb-12'
+          required
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
+        />
+        <Button type='submit' gradientDuoTone='purpleToPink'>
+          Publish
+        </Button>
+        {publishError && (
           <Alert className='mt-5' color='failure'>
             {publishError}
           </Alert>
         )}
-        </form>
+      </form>
     </div>
-  )
+  );
 }
