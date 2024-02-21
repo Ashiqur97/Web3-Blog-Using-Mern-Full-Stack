73a83687a7912cc3c728532c123d9bf508c3efcb
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-21 12:27:45 +0600

    user post

--- src/components/DashPosts.jsx
+++ src/components/DashPosts.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react"
+import {useSelector} from "react-redux"
+
 export default function DashPosts() {
+  const {currentUser} = useSelector(state => state.user)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("/api/post/getPosts?userId=1")
+        const data = await res.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  },[])
   return (
     <div>
         DashPost
