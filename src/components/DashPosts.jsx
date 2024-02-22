3223680462c6b5920d4516067c3155fb2c5f1ac7
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-22 16:36:41 +0600

    initial code of show more functionality

--- src/components/DashPosts.jsx
+++ src/components/DashPosts.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Table, TableHeadCell } from "flowbite-react";
 import { useEffect,useState } from "react"
 import {useSelector} from "react-redux"
@@ -28,6 +29,18 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
         {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -93,6 +106,7 @@ export default function DashPosts() {
           </Table>
             {showMore && (
               <button
+                onClick={handleShowMore}
                 className='w-full text-teal-500 self-center text-sm py-7'
               >
                 Show more
