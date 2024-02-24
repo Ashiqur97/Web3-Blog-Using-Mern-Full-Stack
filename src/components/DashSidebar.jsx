6fbb12ad08e5338d0c9429a9d06c093b6085ed38
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-24 18:51:07 +0600

    show users to the admin dashboard

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,5 +1,7 @@
 import { Sidebar } from 'flowbite-react';
-import {HiArrowSmRight,HiDocumentText, HiUser} from 'react-icons/hi';
+import {HiArrowSmRight,HiDocumentText, 
+  HiAnnotation,HiOutlineUserGroup, 
+  HiUser} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import {Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -59,6 +61,28 @@ export default function DashSidebar() {
                 Posts
               </Sidebar.Item>
             </Link>
+          )}
+              {currentUser.isAdmin && (
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
             <Sidebar.Item icon={HiArrowSmRight} 
             className='cursor-pointer' onClick={handleSignout}>
