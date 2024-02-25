93e796353bf14d672622e0769db1cb1f79806d73
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-26 02:12:49 +0600

    complete post page functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,9 +4,9 @@ import {create,getposts,deletepost,updatepost} from '../controllers/post.control
 
 const router = express.Router();
 
-router.post('/create',verifyToken,create)
-router.get('/getposts',getposts)
-router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
-router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
+router.post('/create', verifyToken, create)
+router.get('/getposts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router;
