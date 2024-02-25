9dfb785e6101d8f44167e1ddb775a9fa94cef18b
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-25 15:57:30 +0600

    complete post page functionality

---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+
+export default function PostPage() {
+    const {postSlug} = useParams();
+
+    useEffect(() => {
+        console.log(postSlug);
+    },[postSlug])
+    
+  return (
+    <div>
+        Post Page       
+    </div>
+  )
+}
