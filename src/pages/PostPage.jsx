f52832ffda71c28fa13874cdcf199a3ad1b3c87c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-26 01:12:48 +0600

    complete post page functionality

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -36,10 +36,21 @@ export default function PostPage() {
         <Spinner size="xl" />
     </div>
     )
-    
+
   return (
-    <div>
-        Post Page       
-    </div>
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+        <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+        {post && post.title}
+      </h1>
+    </main>
   )
 }
+
+{/* <Link
+to={`/search?category=${post && post.category}`}
+className='self-center mt-5'
+>
+<Button color='gray' pill size='xs'>
+  {post && post.category}
+</Button>
+</Link> */}
