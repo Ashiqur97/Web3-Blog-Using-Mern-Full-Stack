0509eec052c4afaa77e32ccf8dd989ad670e6862
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-25 16:04:29 +0600

    complete post page functionality

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,13 +1,28 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function PostPage() {
     const {postSlug} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [post, setPost] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        console.log(postSlug);
+        const fetchPost = async () => {
+           try {
+            const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+            const data = await res.json();
+            if(!res.ok){
+                setError(true);
+                setLoading(false);
+                return;
+            }
+           } catch (error) {
+
+           }
+        }
     },[postSlug])
-    
+
   return (
     <div>
         Post Page       
