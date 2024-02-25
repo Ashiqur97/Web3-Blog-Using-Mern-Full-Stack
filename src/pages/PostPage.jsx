53dcd38b6e6f60c790e3d977697cc8d46616c817
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-26 01:57:00 +0600

    complete post page functionality

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -58,6 +58,9 @@ className='self-center mt-5'
       />
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className='italic'>
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
         </div>
     </main>
   )
