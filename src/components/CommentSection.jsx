b9d63b5bdca9b4df84306d467cd441d97eb8f437
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-28 15:17:33 +0600

    comment section to the post page

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,7 +1,22 @@
-export default function CommentSection() {
+import {useSelector} from 'react-redux';
+
+export default function CommentSection(postId) {
+    const {currentUser} = useSelector(state => state.user);
   return (
     <div>
-        Comment Section 
+        { currentUser ? 
+        (
+            <div className=''>
+                <p>Signed in as:</p>
+                <img src={currentUser.profilePicture} alt=''/>
+                <Link to={'/dashboard?tag=profile'}>
+                    @ {currentUser.username}
+                </Link>
+                </div>
+        ) 
+        :
+        ()
+    }
     </div>
   )
 }
