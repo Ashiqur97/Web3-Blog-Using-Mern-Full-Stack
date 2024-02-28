8024729042075dff153bcd20a52bfb213667aa85
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-28 15:33:48 +0600

    comment section to the post page

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -4,22 +4,23 @@ import { Link } from 'react-router-dom';
 export default function CommentSection(postId) {
     const {currentUser} = useSelector(state => state.user);
   return (
-    <div>
+    <div className='max-w-2xl mx-auto w-full p-3'>
         { currentUser ? 
         (
-            <div className=''>
+            <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
                 <p>Signed in as:</p>
-                <img src={currentUser.profilePicture} alt=''/>
-                <Link to={'/dashboard?tag=profile'}>
+                <img  className='h-5 w-5 object-cover rounded-full' 
+                src={currentUser.profilePicture} alt=''/>
+                <Link className='text-blue-500 hover:underline' to={'/dashboard?tag=profile'}>
                     @ {currentUser.username}
                 </Link>
                 </div>
         ) 
         :
         (
-            <div className=''>
+            <div className='text-sm text-teal-500 my-5 flex gap-1'>
                     you must be signed in to comment
-                    <Link to={'/sign-in'}>
+                    <Link className='text-blue-500 hover:underline' to={'/sign-in'}>
                         Sign In 
                     </Link>
                 </div>
