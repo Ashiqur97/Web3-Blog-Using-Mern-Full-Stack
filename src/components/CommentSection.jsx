935d4023bc973cba5e5a67f239fc008a0d07f59a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-02-29 02:26:52 +0600

    comment section to post page

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -3,9 +3,12 @@
 import { TextInput, Button,Textarea } from 'flowbite-react';
 import {useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function CommentSection({postId}) {
     const {currentUser} = useSelector(state => state.user);
+    const [comment,setComment] = useState('');
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         { currentUser ? 
@@ -30,13 +33,18 @@ export default function CommentSection({postId}) {
         )
     }
     {currentUser && (
-        <form>
+        <form className='border border-teal-500 rounded-md p-3'>
             <Textarea 
             placeholder='Add a comment' 
             rows='3'
-            maxLength='200' />
-            <div className=''>
-                <p>200 character remaining</p>
+            maxLength='200' 
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+            />
+            <div className='flex justify-between items-center mt-5'>
+                <p className='text-gray-500 text-xs'>
+                    200 character remaining
+                    </p>
                 <Button outline 
                 gradientDuoTone='purpleToBlue' 
                 type='submit'>
