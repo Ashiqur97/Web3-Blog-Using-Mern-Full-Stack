8fbf22476d3ed2e336ed98c47df4d6c57071533a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-01 16:36:19 +0600

    comment show

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,30 +1,39 @@
-/* eslint-disable no-unused-vars */
-import { Alert,Button, Modal, TextInput } from "flowbite-react"
-import { useEffect, useRef,useState } from "react";
-import { useSelector } from "react-redux"
-import {getStorage,ref, uploadBytesResumable,getDownloadURL} from 'firebase/storage';
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { app } from '../firebase';
-import { updateStart,updateSuccess,updateFailure,
-  deleteUserFailure,deleteUserStart,deleteUserSuccess,signoutSuccess } from "../redux/user/userSlice";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signoutSuccess,
+} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { HiOutlineExclamationCircle } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const {currentUser,error,loading} = useSelector(state => state.user)
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
-  const [updateUserSuccess, setUpdateUserSuccess] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
   const [updateUserError, setUpdateUserError] = useState(null);
-  const [showModal,setShowModal] = useState(false);
-
+  const [showModal, setShowModal] = useState(false);
   const [formData, setFormData] = useState({});
-
   const filePickerRef = useRef();
   const dispatch = useDispatch();
   const handleImageChange = (e) => {
@@ -122,7 +131,6 @@ export default function DashProfile() {
       setUpdateUserError(error.message);
     }
   };
-
   const handleDeleteUser = async () => {
     setShowModal(false);
     try {
@@ -140,7 +148,7 @@ export default function DashProfile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
-  
+
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -156,17 +164,22 @@ export default function DashProfile() {
       console.log(error.message);
     }
   };
-
   return (
-    <div className="max-w-lg mx-auto p-3 w-full">
-      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden/>
-        <div 
-        className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" 
-        onClick={() => filePickerRef.current.click()}>
-
-{imageFileUploadProgress && (
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
             <CircularProgressbar
               value={imageFileUploadProgress || 0}
               text={`${imageFileUploadProgress}%`}
@@ -187,53 +200,58 @@ export default function DashProfile() {
               }}
             />
           )}
-        <img src={imageFileUrl || currentUser.profilePicture} alt="user" 
-          className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
-            imageFileUploadProgress &&
-            imageFileUploadProgress < 100 &&
-            'opacity-60'
-          }`}/>
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              'opacity-60'
+            }`}
+          />
         </div>
         {imageFileUploadError && (
-            <Alert color='failure'>
-              {imageFileUploadError}
-            </Alert>
+          <Alert color='failure'>{imageFileUploadError}</Alert>
         )}
-        
-        <TextInput 
-        type="text" 
-        id="username" 
-        placeholder="username" 
-        defaultValue={currentUser.username}
-        onChange={handleChange}
+        <TextInput
+          type='text'
+          id='username'
+          placeholder='username'
+          defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
-          type="email"
-          id="email"
-          placeholder="email"
+          type='email'
+          id='email'
+          placeholder='email'
           defaultValue={currentUser.email}
           onChange={handleChange}
         />
-        <TextInput 
-          type="password"
-          id="password"
-          placeholder="password"
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline disabled={loading || imageFileUploading}>
+        <Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+          disabled={loading || imageFileUploading}
+        >
           {loading ? 'Loading...' : 'Update'}
-          </Button>
-          {
-            currentUser.isAdmin && (
-              <Link to={'/create-post'}>
-                      <Button type="button" 
-              gradientDuoTone='purpleToBlue' className="w-full">
-                Create a post 
-              </Button>
-              </Link>
-        
-            )
-          }
+        </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>
@@ -258,24 +276,26 @@ export default function DashProfile() {
           {error}
         </Alert>
       )}
-
-      <Modal show={showModal} 
-      onClose={() => setShowModal(false)} 
-      popup
-      size='md'
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
-              <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
-              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete your account?
             </h3>
-            <div className="flex justify-center gap-4">
-            <Button color='failure' onClick={handleDeleteUser}>
-                Yes, Im sure
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I'm sure
               </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>No,Cancel</Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
             </div>
           </div>
         </Modal.Body>
@@ -283,5 +303,3 @@ export default function DashProfile() {
     </div>
   );
 }
-
-
