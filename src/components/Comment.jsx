1260ed9875f0940ded611d0878780361affe6207
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-02 00:24:13 +0600

    comment of a post

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 
 export default function Comment({ comment}) {
+    const [user,setUser] = useState({});
+    
     useEffect(() => {
         const getUser = async () => {
-            try {
-              
-            } catch (error) {
-                console.log(error.message);
+          try {
+            // eslint-disable-next-line react/prop-types
+            const res = await fetch(`/api/user/${comment.userId}`);
+            const data = await res.json();
+            if (res.ok) {
+              setUser(data);
             }
-        }
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
         getUser();
-    },[comment])
+      }, [comment]);
   return (
     <div>
+        <div className="">
+            <img src={user.profilePicture} alt={user.username} />
+        </div>
     </div>
   )
 }
