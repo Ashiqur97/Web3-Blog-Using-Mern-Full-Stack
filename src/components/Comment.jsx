b2c8403c3eefa6a42cd6082f02a327c8e9eadd45
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-02 00:03:18 +0600

    comment of a post

---
+++ src/components/Comment.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+
+export default function Comment({ comment}) {
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+              
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        getUser();
+    },[comment])
+  return (
+    <div>
+    </div>
+  )
+}
