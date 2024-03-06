7f35a2118a5154e6bc7e2e6db16e4a91f9940771
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-06 12:18:42 +0600

    like functionality to the comment component

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { Alert, TextInput, Button, Textarea } from 'flowbite-react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 // import { get } from 'mongoose';
 import Comment from './Comment';
@@ -12,6 +12,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -57,7 +58,31 @@ export default function CommentSection({ postId }) {
   }, [postId]);
 
   const handleLike = async (commentId) => {
-
+    try {
+      if(!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   return (
