ce6015574ae05917e4c690b7d3107175723f9666
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-11 12:19:58 +0600

    comment section to admin dashboard

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -37,8 +37,7 @@ export const getPostComments = async (req, res, next) => {
 export const likeComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
-
-    if(!comment) {
+    if (!comment) {
       return next(errorHandler(404, 'Comment not found'));
     }
     const userIndex = comment.likes.indexOf(req.user.id);
@@ -54,43 +53,59 @@ export const likeComment = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const editComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
-    if(!comment) {
+    if (!comment) {
       return next(errorHandler(404, 'Comment not found'));
     }
-    if(comment.userId !== req.user.id && !req.user.isAdmin) {
-      return next(errorHandler(403, 'You are not allowed to edit this comment'));
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, 'You are not allowed to edit this comment')
+      );
     }
 
     const editedComment = await Comment.findByIdAndUpdate(
       req.params.commentId,
-      { 
-        content:req.body.content 
+      {
+        content: req.body.content,
       },
       { new: true }
     );
     res.status(200).json(editedComment);
   } catch (error) {
-      next(error);
+    next(error);
   }
-}
+};
 
 export const deleteComment = async (req, res, next) => {
   try {
-    if(!comment){
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
       return next(errorHandler(404, 'Comment not found'));
     }
-    if(comment.userId !== req.user.id && !req.user.isAdmin) {
-      return next(errorHandler(403, 'You are not allowed to delete this comment'));
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, 'You are not allowed to delete this comment')
+      );
     }
     await Comment.findByIdAndDelete(req.params.commentId);
-    res.status(200).json('Comment deleted successfully');
+    res.status(200).json('Comment has been deleted');
   } catch (error) {
     next(error);
   }
+};
 
+export const getcomments = async (req, res, next) => {
+  if (!req.user.isAdmin)
+  return next(errorHandler(403, 'You are not allowed to get all comments'));
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'desc' ? -1 : 1;
+  } catch (error) {
+    next(error);
+  }
 }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js';
 import { createComment,getPostComments,likeComment,editComment,deleteComment } from '../controllers/comment.controller.js';
+import { get } from 'mongoose';
 
 const router = express.Router();
 
-router.post('/create', verifyToken,createComment);
+router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
-router.put('/likeComment/:commentId', verifyToken,likeComment);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
-router.put('/deleteComment/:commentId', verifyToken, deleteComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments',getComments);
 
 export default router;
+
