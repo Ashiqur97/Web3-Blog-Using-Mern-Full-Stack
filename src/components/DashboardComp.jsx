4fae991e15e1e876164260af3ee7ab5b621c0bf3
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-16 15:32:58 +0600

    dashboard to admin dashboard

--- src/components/DashboardComp.jsx
+++ src/components/DashboardComp.jsx
@@ -65,22 +65,23 @@ export default function DashboardComp() {
         }, [currentUser]);
 
   return (
-    <div>
-        <div className="">
-            <div className="">
-                <div className="">
-                    <h3>Total Users</h3>
+    <div className="p-3 md:mx-auto">
+        <div className="flex-wrap flex gap-4 justify-center">
+            <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+                <div className='flex justify-between'>
+                    <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
                     <p className='text-2xl'>{totalUsers}</p>
-                    <HiOutlineUserGroup className='bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+                    
                 </div>
-                <div className="flex gap-2 text-sm">
-                    <span className="text-green-500 flex items-center">
+                <HiOutlineUserGroup className='bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+            </div>
+            <div className="flex gap-2 text-sm">
+                    <span className='text-green-500 flex items-center'>
                         <HiArrowNarrowUp />
                         {lastMonthUsers}
                     </span>
-                    <div className="">Last Month</div>
+                    <div className='text-gray-500'>Last Month</div>
                 </div>
-            </div>
         </div>
     </div>
   )
