f435a3408e84cecba3464fbbe1d524b07fb6078d
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-16 16:03:50 +0600

    dashboard to admin dashboard

--- src/components/DashboardComp.jsx
+++ src/components/DashboardComp.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { current } from "@reduxjs/toolkit";
 import { useEffect, useState } from "react";
-import { HiArrowNarrowUp, HiOutlineUserGroup } from "react-icons/hi";
+import { HiArrowNarrowUp, HiOutlineUserGroup,HiAnnotation,HiDocumentText } from "react-icons/hi";
 import { useSelector } from "react-redux";
 
 export default function DashboardComp() {
@@ -66,42 +66,58 @@ export default function DashboardComp() {
 
     return (
         <div className='p-3 md:mx-auto'>
-            <div className='flex-wrap flex gap-4 justify-center'>
-                <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
-                    <div className='flex justify-between'>
-                        <div className=''>
-                            <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
-                            <p className='text-2xl'>{totalUsers}</p>
-                        </div>
-                        <HiOutlineUserGroup className='bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg' />
-                    </div>
-                    <div className='flex  gap-2 text-sm'>
-                        <span className='text-green-500 flex items-center'>
-                            <HiArrowNarrowUp />
-                            {lastMonthUsers}
-                        </span>
-                        <div className='text-gray-500'>Last month</div>
-                    </div>
-                </div>
-                <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
-                    {/* <div className='flex justify-between'>
-          <div className=''>
-            <h3 className='text-gray-500 text-md uppercase'>
-              Total Comments
-            </h3>
-            <p className='text-2xl'>{totalComments}</p>
-          </div>
-          <HiAnnotation className='bg-indigo-600  text-white rounded-full text-5xl p-3 shadow-lg' />
-        </div> */}
-                    {/* <div className='flex  gap-2 text-sm'>
-          <span className='text-green-500 flex items-center'>
-            <HiArrowNarrowUp />
-            {lastMonthComments}
-          </span>
-          <div className='text-gray-500'>Last month</div>
-        </div> */}
-                </div>
+      <div className='flex-wrap flex gap-4 justify-center'>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
+              <p className='text-2xl'>{totalUsers}</p>
             </div>
+            <HiOutlineUserGroup className='bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthUsers}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
         </div>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>
+                Total Comments
+              </h3>
+              <p className='text-2xl'>{totalComments}</p>
+            </div>
+            <HiAnnotation className='bg-indigo-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthComments}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
+        </div>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>Total Posts</h3>
+              <p className='text-2xl'>{totalPosts}</p>
+            </div>
+            <HiDocumentText className='bg-lime-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthPosts}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
+        </div>
+      </div>
+      </div>
     )
 }
