eb3939d0932eafdbd1dbad1d681776110d415359
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-03-31 03:04:13 +0600

    complete search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='reactjs'>Web3 && Solidity</option>
+              <option value='nextjs'>Ethereum && DEFI</option>
+              <option value='javascript'>Smart Contract Audit</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
